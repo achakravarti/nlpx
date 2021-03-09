6a2bf93b7c494bd7e6d153bfa58ada66bc3fcfa8
Abhishek Chakravarti <abhishek@taranjali.org>
2021-03-09 22:40:27 +0530

    Rename _pos table to _upenn_treebank_tagset
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- sql/pos.sql
+++ sql/pos.sql
@@ -1,91 +1,163 @@
 SET SEARCH_PATH TO PUBLIC;
 
 
-DROP TABLE IF EXISTS _pos CASCADE;
+DROP TABLE IF EXISTS _upenn_treebank_tagset CASCADE;
 
 
-CREATE TABLE IF NOT EXISTS _pos(
+-- https://stackoverflow.com/questions/15388831
+-- https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+
+CREATE TABLE IF NOT EXISTS _upenn_treebank_tagset(
 	id		BIGSERIAL PRIMARY KEY,
-	name		VARCHAR(5) NOT NULL UNIQUE,
+	tag		VARCHAR(5) NOT NULL UNIQUE,
 	description	VARCHAR(64) NOT NULL,
 	example		VARCHAR(64)
 );
 
 
 BEGIN;
-	INSERT INTO _pos(name, description, example) VALUES('CC',
-	    'coordinating conjunction', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('CD',
-	    'cardinal digit', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('DT',
-	    'determiner', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('EX',
-	    'existential there', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('FW',
-	    'foreign word', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('IN',
-	    'preposition/subordinating conjunction', NULL)
-	    ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('JJ',
-	    'adjective', 'large') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('JJR',
-	    'adjective, comparative',  'larger') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('JJS',
-	    'adjective, superlative', 'largest') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('LS',
-	    'list marker', NULL) ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('MD',
-	    'modal',  'could, will') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('NN',
-	    'noun, singular', 'cat, tree') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('NNS',
-	    'noun, plural', 'desks') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('NNP',
-	    'proper noun, singular', 'Sarah') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('NNPS',
-	    'proper noun, plural', 'Indians or Americans')
-            ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('PDT',
-	    'predeterminer', 'all, both, half') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('POS',
-	    'possessive ending', 'parent''s') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('PRP',
-	    'personal pronoun', 'hers, herself, him,himself')
-	    ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('PRP$',
-	    'possessive pronoun', 'her, his, mine, my, our')
-            ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('RB',
-	    'adverb',  'occasionally, swiftly') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('RBR',
-	    'adverb, comparative', 'greater') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('RBS',
-	    'adverb, superlative', 'biggest') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('RP',
-	    'particle', 'about') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('TO',
-	    'infinite marker', 'to') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('UH',
-	    'interjection', 'goodbye') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VB',
-	    'verb', 'ask') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VBG',
-	    'verb gerund', 'judging') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VBD',
-	    'verb past tense', 'pleaded') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VBN',
-	    'verb past participle', 'reunified') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VBP',
-	    'verb, present tense not 3rd person singular', 'wrap')
-            ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('VBZ',
-	    'verb, present tense with 3rd person singular', 'bases')
-            ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('WDT',
-	    'wh-determiner', 'that, what') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('WP',
-	    'wh- pronoun', 'who') ON CONFLICT DO NOTHING;
-	INSERT INTO _pos(name, description, example) VALUES('WRB',
-	    'wh-adverb', 'how') ON CONFLICT DO NOTHING;
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'CC', 'Coordinating conjunction', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'CD', 'Cardinal number', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'DT', 'Determiner', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'EX', 'Existential ''there''', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'FW', 'Foreign word', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'IN', 'Preposition or subordinating conjunction', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'JJ', 'Adjective', 'large'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'JJR', 'Adjective, comparative',  'larger'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'JJS', 'Adjective, superlative', 'largest'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'LS', 'List item marker', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'MD', 'Modal',  'could, will'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'NN', 'Noun, singular or mass', 'cat, tree'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'NNS', 'Noun, plural', 'desks'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'NNP', 'Proper noun, singular', 'Sarah'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'NNPS', 'Proper noun, plural', 'Indians or Americans'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'PDT', 'Predeterminer', 'all, both, half'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'POS', 'Possessive ending', 'parent''s'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'PRP', 'Personal pronoun', 'hers, herself, him,himself'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+		'PRP$', 'Possessive pronoun', 'her, his, mine, my, our'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'RB', 'Adverb',  'occasionally, swiftly'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'RBR', 'Adverb, comparative', 'greater'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'RBS', 'Adverb, superlative', 'biggest'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'RP', 'Particle', 'about'
+	) ON CONFLICT DO NOTHING;
+	
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'SYM', 'Symbol', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'TO', 'Infinitive marker', 'to'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'UH', 'Interjection', 'goodbye'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VB', 'Verb, base form', 'ask'
+	) ON CONFLICT DO NOTHING;
+	
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VBD', 'Verb, past tense', 'pleaded'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VBG', 'Verb, gerund', 'judging'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VBN', 'Verb, past participle', 'reunified'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VBP', 'Verb, non-3rd person singular present', 'wrap'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'VBZ', 'Verb, 3rd person singular present', 'bases'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'WDT', 'Wh-determiner', 'that, what'
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'WP', 'Wh- pronoun', 'who'
+	) ON CONFLICT DO NOTHING;
+	
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'WP$', 'Possessive wh- pronoun', NULL
+	) ON CONFLICT DO NOTHING;
+
+	INSERT INTO _upenn_treebank_tagset(tag, description, example) VALUES(
+	    'WRB', 'Wh-adverb', 'how'
+	) ON CONFLICT DO NOTHING;
 END;
 
