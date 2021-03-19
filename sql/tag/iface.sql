8ec3b7855bd863a1506c78f98772ef7cb472a1c2
Abhishek Chakravarti <abhishek@taranjali.org>
2021-03-20 01:11:53 +0530

    Define tags.entity_search()
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- sql/tag/iface.sql
+++ sql/tag/iface.sql
@@ -41,7 +41,7 @@ create or replace function tags.pos_search(
 ) returns setof tags.pos
 as $$
 begin
-	return query select id, cat, tag, def from tags.pos where tag % tag_ 
+	return query select id, cat, tag, def from tags.pos where tag % tag_
 		order by cat, tag;
 end;
 $$ language plpgsql;
@@ -51,7 +51,7 @@ create or replace procedure tags.pos_add_universal(
 	tag_	text,
 	def_	text
 ) as $$
-	insert into tags.pos(cat, tag, def) 
+	insert into tags.pos(cat, tag, def)
 		values ('UNIVERSAL', tag_, def_)
 		on conflict do nothing;
 $$ language sql;
@@ -61,7 +61,7 @@ create or replace procedure tags.pos_add_penn_treebank(
 	tag_	text,
 	def_	text
 ) as $$
-	insert into tags.pos(cat, tag, def) 
+	insert into tags.pos(cat, tag, def)
 		values ('PENN_TREEBANK', tag_, def_)
 		on conflict do nothing;
 $$ language sql;
@@ -78,7 +78,7 @@ begin
 		where id = id_;
 
 	if not found then
-		RAISE EXCEPTIon 'PoS tags.with ID % not available', id_;
+		raise exception 'PoS tags.with ID % not available', id_;
 	end if;
 end;
 $$ language plpgsql;
@@ -111,8 +111,8 @@ create or replace function tags.dependency_search(
 ) returns setof tags.dependency
 as $$
 begin
-	return query select id, tag, def from tags.dependency
-		where tag % tag_ order by tag;
+	return query select id, tag, def from tags.dependency where tag % tag_
+		order by tag;
 end;
 $$ language plpgsql;
 
@@ -158,6 +158,17 @@ create or replace function tags.entity_find(
 ) as $$
 begin
 	return query select tags.entity.id from tags.entity where tag = tag_;
+end;
+$$ language plpgsql;
+
+
+create or replace function tags.entity_search(
+	_tag	text
+) returns setof tags.entity
+as $$
+begin
+	return query select id, tag, def from tags.entity where tag % tag_
+		order by tag;
 end;
 $$ language plpgsql;
 
