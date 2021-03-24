c55f25834149ab58da5bf14f04c23fffdcc6390f
Abhishek Chakravarti <abhishek@taranjali.org>
2021-03-24 19:46:02 +0530

    Improve corpus queries
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- corpus/iface.sql
+++ corpus/iface.sql
@@ -6,7 +6,12 @@ create or replace function title_single(
 ) returns setof title
 as $$
 begin
-	return query select id, title, author from corpus.title where id = p_id;
+	return query 
+	select	T.id, 
+		T.title, 
+		T.author 
+	from corpus.title as T
+	where T.id = p_id;
 
 	if not found then
 		raise exception 'title with ID % not available', p_id;
@@ -18,7 +23,11 @@ $$ language plpgsql;
 create or replace function title_all()
 returns setof title
 as $$
-	select id, title, author from corpus.title order by title;
+	select	T.id, 
+		T.title, 
+		T.author 
+	from corpus.title as T
+	order by T.title;
 $$ language sql;
 
 
@@ -27,26 +36,31 @@ create or replace function title_find(
 ) returns table (
 	id	int
 ) as $$
-	select corpus.title.id from corpus.title where title = p_title;
+	select T.id 
+	from corpus.title as T
+	where T.title = p_title;
 $$ language sql;
 
 
 create or replace function title_search(
 	p_title	text
-) returns setof title
+) returns setof corpus.title
 as $$
-begin
-	return query select id, title, author from corpus.title
-		where title.title % p_title order by title;
-end;
-$$ language plpgsql;
+	select	T.id, 
+		T.title, 
+		T.author 
+	from corpus.title as T
+	where T.title % p_title 
+	order by T.title;
+$$ language sql;
 
 
 create or replace procedure title_add(
 	p_title		text,
 	p_author	text
 ) as $$
-insert into corpus.title (title, author) values (p_title, p_author)
+	insert into corpus.title (title, author) 
+	values (p_title, p_author)
 	on conflict do nothing;
 $$ language sql;
 
@@ -59,10 +73,13 @@ create or replace function token_single(
 	pos	text
 ) as $$
 begin
-	return query select corpus.token.id, corpus.token.lexeme::text,
-		tags.pos.tag::text as pos from corpus.token 
-		inner join tags.pos on corpus.token.pos = tags.pos.id
-		where corpus.token.id = p_id;
+	return query
+	select	T.id, 
+		T.lexeme::text, 
+		P.tag::text as pos 
+	from corpus.token as T
+	inner join tags.pos as P on T.pos = P.id
+	where T.id = p_id;
 	
 	if not found then
 		raise exception 'token with ID % not available', p_id;
@@ -77,9 +94,11 @@ returns table (
 	lexeme	text,
 	pos	text
 ) as $$
-	select corpus.token.id, corpus.token.lexeme::text, 
-		tags.pos.tag::text as pos from corpus.token 
-		inner join tags.pos on corpus.token.pos = tags.pos.id;
+	select	T.id, 
+		T.lexeme::text, 
+		P.tag::text as pos 
+	from corpus.token as T
+	inner join tags.pos as P on T.pos = P.id;
 $$ language sql;
 
 
@@ -89,17 +108,27 @@ create or replace function token_find(
 ) returns table (
 	id	int
 ) as $$
-	select corpus.token.id from corpus.token where lexeme = p_lexeme
-		and pos = (select id from tags.pos_find(p_pos));
+	select T.id 
+	from corpus.token as T
+	where T.lexeme = p_lexeme
+	and T.pos = (select id from tags.pos_find(p_pos));
 $$ language sql;
 
 
 create or replace function token_search(
 	p_lexeme	text
-) returns setof token
-as $$
-	select id, lexeme, pos from corpus.token where lexeme % p_lexeme
-		order by lexeme;
+) returns table (
+	id	int,
+	lexeme	text,
+	pos	text
+) as $$
+	select	T.id, 
+		T.lexeme::text,
+		P.tag::text as pos
+	from corpus.token as T
+	inner join tags.pos as P on T.pos = P.id
+	where T.lexeme % p_lexeme
+	order by T.lexeme;
 $$ language sql;
 
 
@@ -107,8 +136,9 @@ create or replace procedure token_add(
 	p_lexeme	text,
 	p_pos		text
 ) as $$
-	insert into corpus.token (lexeme, pos) values (p_lexeme, 
-		tags.pos_find(p_pos)) on conflict do nothing;
+	insert into corpus.token (lexeme, pos) 
+	values (p_lexeme, tags.pos_find(p_pos)) 
+	on conflict do nothing;
 $$ language sql;
 
 
